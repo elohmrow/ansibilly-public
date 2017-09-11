712bac939f4100e18c3d4fc4469bd0dba0281331
Bradley Andersen <bradley.andersen@magnolia-cms.com>
2017-09-11 08:48:06 -0400

    modify ami util script add systemd unit (for reference)

--- utils/thundercats.sh
+++ utils/thundercats.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# get the code we need to build the magnolis instances (could do differently):
+cd /home/ec2-user
+
 git clone ssh://git@git.magnolia-cms.com/training/training-author-instances-creation.git
 
-# get the utility scripts we need for this part:
 cp training-author-instances-creation/utils/* .
 
-# do some cleanup (not really necessary):
 rm -rf training-author-instances-creation README
 
-# now actually run everything (see the docs, yo!):
-./install_magnolia.sh 
+./install_magnolia.sh
 
-# after install_magnolia runs, we have a bag o' stuff we no longer need ... clean it up:
 rm -rf author-bootstraps-1.0-SNAPSHOT.jar fiddle_the_war.sh get_most_recent_magnolia.sh get_tomcat.sh install_magnolia.sh training-author-instances-creation
-
